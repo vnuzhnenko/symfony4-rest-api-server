22171841b56a29c2ebcd3528bbdde63e2025ccb8
Viktor Nuzhnenko <victor.nuzhnenko@gmail.com>
2018-08-19 22:28:50 +0300

    A very first version of API for the tasks handling

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -4,13 +4,15 @@ namespace App\Controller;
 
 use App\Entity\Task;
 use App\Repository\TaskRepository;
-use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\Query;
+use App\Form\TaskType;
+use App\Service\TaskService;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations\RouteResource;
 use FOS\RestBundle\Controller\Annotations;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -24,51 +26,116 @@ class TaskController extends FOSRestController
     private $taskRepository;
 
     /**
-     * TaskController constructor.
+     * @var TaskService
+     */
+    private $taskService;
+
+    /**
+     * TaskController constructor
+     * @param TaskRepository $taskRepository
+     * @param TaskService $taskService
      */
     public function __construct(
-        TaskRepository $taskRepository
+        TaskRepository $taskRepository,
+        TaskService $taskService
     ) {
         $this->taskRepository = $taskRepository;
+        $this->taskService = $taskService;
     }
 
     /**
-     * @Annotations\Get(path="/task/{id}")
+     * @Route("/ping")
      * @return JsonResponse
      */
-//    public function ping()
+//    public function ping(Request $request)
 //    {
-//        return new JsonResponse('pong');
+//        return JsonResponse::create('pong');
 //    }
 
-
     /**
-     * @param $id int
+     * @param string $id
      * @return JsonResponse
      */
-    public function getAction($id)
+    public function getAction(string $id)
     {
-        return JsonResponse::create($this->taskRepository->find($id), JsonResponse::HTTP_OK);
+        $task = $this->taskRepository->find($id);
+        if (null !== $task) {
+            $view = $this->view($task, Response::HTTP_OK);
+        } else {
+//            throw new NotFoundHttpException();
+            $view = $this->view(['status' => 'Task not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        return $this->handleView($view);
+    }
+
+    /**
+     * @return Response
+     */
+    public function cgetAction()
+    {
+        $view = $this->view(
+            $this->taskRepository->findAll()
+        );
+        return $this->handleView($view);
     }
 
     /**
      * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function postAction(Request $request)
     {
+        $form = $this->createForm(TaskType::class, new Task);
+        $form->submit($request->request->all());
+
+        if (false === $form->isValid()) {
+            return $this->handleView($this->view($form));
+        }
+
+        if ($this->taskService->create($form->getData())) {
+            return $this->handleView(
+                $this->view(['status' => 'ok'], Response::HTTP_CREATED)
+            );
+        }
+    }
+
+    /**
+     * @param string $id
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function deleteAction(string $id)
+    {
+        $task = $this->taskRepository->find($id);
+        $this->taskService->delete($task);
+
+        $view = $this->view(null, Response::HTTP_NO_CONTENT);
+        return $this->handleView($view);
     }
 
     /**
      * @param Request $request
+     * @param string $id
+     * @return Response
      */
-    public function deleteAction(Request $request)
+    public function putAction(Request $request, string $id)
     {
-    }
+        $task = $this->taskRepository->find($id);
 
-    /**
-     * @param Request $request
-     */
-    public function putAction(Request $request)
-    {
+        if (null === $task) {
+            throw new NotFoundHttpException();
+        }
+
+        $form = $this->createForm(TaskType::class, $task);
+        $form->submit($request->request->all());
+
+        if (false === $form->isValid()) {
+            return $this->handleView($this->view($form));
+        }
+
+        $this->taskService->modify($task);
+
+        $view = $this->view(null, Response::HTTP_NO_CONTENT);
+        return $this->handleView($view);
     }
 }
---
+++ src/Service/TaskService.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Task;
+use Doctrine\ORM\EntityManagerInterface;
+
+class TaskService
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * Creates a new Task
+     * @param Task $task
+     * @return bool
+     */
+    public function create(Task $task): bool
+    {
+        try {
+            $task->setCreatedAt(new \DateTimeImmutable());
+            $this->entityManager->persist($task);
+            $this->entityManager->flush();
+        } catch (\Exception $e) {
+            error_log($e);
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Creates a new Task
+     * @param Task $task
+     * @return bool
+     */
+    public function modify(Task $task): bool
+    {
+        try {
+            // Something useful logic can be here
+            $this->entityManager->flush();
+        } catch (\Exception $e) {
+            error_log($e);
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * @param Task $task
+     * @return bool
+     */
+    public function delete(Task $task): bool
+    {
+        try {
+            $this->entityManager->remove($task);
+            $this->entityManager->flush($task);
+        } catch (\Exception $e) {
+            error_log($e);
+            return false;
+        }
+        return true;
+    }
+}
